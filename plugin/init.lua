5e91f07fd41bb3142cc784a6e54c8b135e76dd2a
Arnau Prat <arnau.prat@gmail.com>
2023-01-27 07:37:44 +0100

    playing with lua

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,22 +1,54 @@
 
-gotoerror = {
-  buffer_data = {}, 
 
-  -- called when openning a buffer
-  on_open_buffer = function(buffer_handle)
-    gotoerror.buffer_data[buffer_handle] = {
-      current_line = 0
-    }
-  end,
-
-  -- called when closing a buffer
-  on_close_buffer = function(buffer_handle)
-    gotoerror.buffer_data[buffer_handle] = nil
-  end,
+local create_buffer_entry = function ()
+return {
+  m_current_line = 0,
+  m_test = 0
 }
+end
 
-gotoerror.on_open_buffer("test")
-print(vim.inspect(gotoerror))
-gotoerror.on_close_buffer("test")
-print(vim.inspect(gotoerror))
+
+local find_buffer_entry  = function(buffer_handle)
+  if gotoerror.m_buffer_data[buffer_handle] == nil 
+    then
+      gotoerror.m_buffer_data[buffer_handle] = create_buffer_entry()
+    end
+    return gotoerror.m_buffer_data[buffer_handle]
+end
+
+
+
+-- called to go to next error instance 
+local next_error_msg = function(buffer_handle)
+  local buffer_data = find_buffer_entry(buffer_handle)
+  print(buffer_data)
+  buffer_data.m_test = buffer_data.m_test + 1
+  gotoerror.m_buffer_data[buffer_handle] = buffer_data
+  end
+
+  -- called to go to previous error instance
+local previous_error_msg = function(buffer_handle)
+  local buffer_data = find_buffer_entry(buffer_handle)
+  buffer_data.m_test = buffer_data.m_test + 1
+  gotoerror.m_buffer_data[buffer_handle] = buffer_data
+  end
+
+  -- called to jump to error place
+  local jump_to_error = function(buffer)
+  end
+
+
+  -- Global object
+  gotoerror = {
+    m_buffer_data = {}, 
+    next_error_msg      = next_error_msg,
+    previous_error_msg  = previous_error_msg,
+    jump_to_error       = jump_to_error,
+
+  }
+
+  gotoerror.next_error_msg("test")
+  print(vim.inspect(gotoerror))
+  gotoerror.previous_error_msg("test")
+  print(vim.inspect(gotoerror))
 
